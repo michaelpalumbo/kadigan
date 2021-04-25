05be1aff49af46c80af46e8960d2b38c42359b65
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2021-04-25 18:41:40 -0400

    write to  text file

--- speech2text.js
+++ speech2text.js
@@ -1,11 +1,28 @@
 const {PythonShell} = require('python-shell');
-
+const fs = require('fs');
+let date = new Date().toISOString().
+  replace(/T/, '_').      // replace T with a space
+  replace(/\..+/, '') 
+const sessionName = 'placeholder_text2speech_'+ date
+const sessionFile = sessionName + '.txt'
 let counter = 0
+
+fs.writeFile(sessionFile, sessionName, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log(`Writing text to new text file: ${sessionFile}\n`);
+    }
+  });
+
 function capture(){
     console.log('speak for the next 5 seconds...')
     PythonShell.run('speech2text.py', null, function (err, results) {
         // if (err) throw err;
         console.log('Text:', results[1]);
+        let text = '\n' + results[1]
+        // add to the file
+        fs.appendFile(sessionFile, text, (err) => {});
         counter++
         capture()
     });
